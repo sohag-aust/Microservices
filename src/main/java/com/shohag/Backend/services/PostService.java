3bcbb54224403c6104bdd0005b9bdefa1a62dac1
sohag-aust <frustratedcoder96@gmail.com>
2022-07-18 19:39:30 +0600

    completed sorting in Posts

--- src/main/java/com/shohag/Backend/services/PostService.java
+++ src/main/java/com/shohag/Backend/services/PostService.java
@@ -10,7 +10,7 @@ public interface PostService {
     PostDto createPost(PostDto postDto, Long userId, Long categoryId);
     PostDto updatePost(PostDto postDto, Long postId);
     void deletePost(Long postId);
-    PostResponse getPosts(Integer pageNo, Integer pageSize);
+    PostResponse getPosts(Integer pageNo, Integer pageSize, String sortBy, String sortOrder);
     PostDto getPostById(Long postId);
     List<PostDto> getPostsByCategory(Long categoryId);
     List<PostDto> getPostsByUser(Long userId);
