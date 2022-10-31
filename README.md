ae9247124a12a2a37f495f530a9c8b7e27481d40
sohag-aust <frustratedcoder96@gmail.com>
2022-10-31 18:02:11 +0600

    Swagger Configuration with Authorize API's by JWT token

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Microservices
 ### Swagger
     `http://localhost:8095/swagger-ui/`
+
+### Swagger implementation with Authorize token for all API's
+    `https://www.javaguides.net/2021/06/configure-jwt-with-spring-boot-and-swagger.html`
--- src/main/java/com/shohag/Backend/configs/SwaggerConfig.java
+++ src/main/java/com/shohag/Backend/configs/SwaggerConfig.java
@@ -4,15 +4,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "Spring Boot Blog REST APIs",
+                "Spring Boot Blog REST API Documentation",
+                "1",
+                "Terms of service",
+                new Contact("Md.Ashraful", "https://github.com/sohag-aust/", "asraful.islam.0496@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList()
+        );
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
